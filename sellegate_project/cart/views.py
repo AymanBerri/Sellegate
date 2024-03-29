abc7466156f4d21835fb90af463d70d9194a4aaa
unknown <berriayman11@gmail.com>
2024-03-29 15:13:53 +0300

    added explanations to the cart view apis

--- sellegate_project/cart/views.py
+++ sellegate_project/cart/views.py
@@ -11,9 +11,31 @@ from item_management.models import Item
 
 
 class AddToCartAPIView(APIView):
+    """
+AddToCartAPIView:
+-----------------
+This API view is responsible for adding an item to the user's cart. 
+"""
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        """
+        Add an item to the user's cart.
+
+        Request Format:
+        ---------------
+        {
+            "item_id": <item_id>,
+            "quantity": <quantity>  # Optional, defaults to 1 if not provided
+        }
+
+        - "item_id": (integer) The ID of the item to be added to the cart.
+        - "quantity": (integer) The quantity of the item to be added to the cart. Optional. If not provided, defaults to 1.
+
+        Returns:
+        --------
+        Returns a JSON response indicating the success or failure of the operation along with the updated cart data if successful.
+        """
         item_id = request.data.get('item_id')  
         quantity = request.data.get('quantity', 1)  # Default quantity is 1 if not provided
 
@@ -41,37 +63,35 @@ class AddToCartAPIView(APIView):
             status_code = status.HTTP_200_OK
 
         return Response({"message": message, "data": cart_serializer.data}, status=status_code)
-
-# class AddToCartAPIView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def post(self, request):
-#         item_id = request.data.get('item_id')  
-#         quantity = request.data.get('quantity', 1)  # Default quantity is 1 if not provided
-
-#         # Retrieve the authenticated user's cart
-#         cart, created = Cart.objects.get_or_create(user=request.user)
-
-#         try:
-#             item = Item.objects.get(id=item_id)  # Get the item based on the provided ID
-#         except Item.DoesNotExist:
-#             return Response({"error": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
-
-#         # Create or update the cart item
-#         cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item)
-#         cart_item.quantity += int(quantity)
-#         cart_item.save()
-
-#         # Serialize the entire cart including its items
-#         cart_serializer = CartSerializer(cart)
-
-#         return Response(cart_serializer.data, status=status.HTTP_201_CREATED)
-    
+ 
 
 class RemoveFromCartAPIView(APIView):
+    """
+    RemoveFromCartAPIView:
+    ----------------------
+    This API view is responsible for removing an item from the user's cart.
+    """
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        """
+        Remove an item from the user's cart.
+
+        Request Format:
+        ---------------
+        {
+            "item_id": <item_id>,
+            "quantity": <quantity>  # Optional, defaults to removing the entire item if not provided
+        }
+
+        - "item_id": (integer) The ID of the item to be removed from the cart.
+        - "quantity": (integer) The quantity of the item to be removed from the cart. Optional. If not provided, defaults to removing the entire item.
+
+        Returns:
+        --------
+        Returns a JSON response indicating the success or failure of the operation along with the updated cart data if successful.
+        """
+
         # Get item_id and quantity from request data
         item_id = request.data.get('item_id')
         quantity = request.data.get('quantity')
